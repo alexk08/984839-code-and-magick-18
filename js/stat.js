1161e484b8ba2aa88cf506a9c4bdb0693d152f17
alexk08 <akapacuk@gmail.com>
2019-10-01 17:06:44 +0400

    часть дз

--- js/stat.js
+++ js/stat.js
@@ -1,12 +1,3 @@
-// window = {
-//   renderStatistics: function(ctx, names, times) {
-
-//   }
-// }
-
-// window.renderStatistics = function (ctx, names,times) {
-
-// }
 'use strict';
 
 var CLOUD_WIDTH = 420;
@@ -42,7 +33,8 @@ window.renderStatistics = function(ctx, players, times) {
 
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', 10, 100);
+  ctx.fillText('Ура вы победили!', CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP);
+  ctx.fillText('Список результатов:', CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP*2);
 
   var maxTime = getMaxElement(times);
 
